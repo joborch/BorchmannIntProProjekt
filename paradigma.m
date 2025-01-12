e0e4973d0c78d2cd74fbf6093579e300332022a3
joborch <s1078397@stud.plus.ac.at>
2025-01-12 13:01:13 +0100

    Vorkommentare eingefügt

---
+++ paradigma.m
@@ -0,0 +1,9 @@
+%author: Jonas Borchmann
+%Description: This file operates the Psychtoolbox Paradigma.
+%Configuration: You dont need to change anything. Changable Settings will be showed in a block at the front.
+
+%% Changable Settings
+
+
+%% Working Directory
+
