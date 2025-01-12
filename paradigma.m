adec0e8247ab9e032505aee570f38f350b4119c3
joborch <s1078397@stud.plus.ac.at>
2025-01-12 14:42:26 +0100

    Tondateien einlesen implementiert
    Pfade für Bitmaps und Töne in Settings eingefügt.
    Einlesen der Tondateien implementiert und abspielbare Buffer in PsychPortAudio programmiert

--- paradigma.m
+++ paradigma.m
@@ -7,6 +7,10 @@
 currentScreen = 1;
 %Change this to your preferred Screen-Ratio for the PTB-Screen (1 = Fullscreen)
 ratioFactor = 0.25;
+%Change this if you want to change the path of the stored Bitmaps
+bitmapPath = '.\bitmaps\';
+%Change this if you want to change the path of the stored Tones
+tonePath = '.\tondateien\';
 
 
 
@@ -28,19 +32,30 @@ ratio = [0 0 width*ratioFactor height*ratioFactor]; %Definition of Ratio for myW
 win = Screen('OpenWindow', currentScreen, black, ratio); %Opens a window on the defined Screen, with the defined ratioFactor (see Changable Setting Section)
 
 %% Einlesen der Bitmaps
-bitmapsFolder = dir('.\bitmaps\'); %Definition of "Bitmaps" Folder
+bitmapsFolder = dir(bitmapPath); %Definition of "Bitmaps" Folder
 
 bitmaps = cell(8,1);
 bitmapTextures = double(8);
 
 for i = 3:10
-    bitmaps{i-2} = imread(fullfile('./bitmaps/',bitmapsFolder(i).name));
+    bitmaps{i-2} = imread(fullfile(bitmapPath ,bitmapsFolder(i).name));
     bitmapTextures(i-2) = Screen('MakeTexture', win, bitmaps{i-2});
 end
 disp("Bitmaps eingelesen!");
 
 %% Einlesen der Tondateien
-tondateienFolder = dir('.\tondateien\'); %Definition of "Tondateien" Folder
+tondateienFolder = dir(tonePath); %Definition of "Tondateien" Folder
+
+wavedata = cell(26,1);
+freq = double(26);
+pahandle = double(26);
+
+for i = 3:28
+    [wavedata{i-2}, freq(i-2)] = audioread(fullfile(tonePath, tondateienFolder(i).name));
+    pahandle(i-2) = PsychPortAudio('Open', [], [], 1, freq(i-2), 1, 0);
+    PsychPortAudio('FillBuffer', pahandle(i-2), wavedata{i-2}');
+end
+disp("Tondateien eingelesen!");
 
 
 KbWait;
