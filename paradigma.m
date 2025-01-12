020154fe1ae36e88b6ca99d1edd9a5a425a84a8b
joborch <s1078397@stud.plus.ac.at>
2025-01-12 13:39:03 +0100

    Working Directory initialisiert

--- paradigma.m
+++ paradigma.m
@@ -3,7 +3,10 @@
 %Configuration: You dont need to change anything. Changable Settings will be showed in a block at the front.
 
 %% Changable Settings
-
+a = 1;
+b = 2;
 
 %% Working Directory
-
+currentFilePath = mfilename('fullpath'); %speichern vom Pfad der genutzten Datei
+[currentFolderPath, ~, ~] = fileparts(currentFilePath); %rausspeichern vom Ordner-Pfad 
+cd(currentFolderPath); %Aktuelles Working Directory setzen!
